04040cc13dfe3aa42a2cbbff6e96064f24439229
Thandar9 <wint.thandar.aye@mic-p.com>
2024-06-03 10:23:17 +0900

    python.exe

---
+++ main.py
@@ -0,0 +1,7 @@
+import my_module
+
+
+
+
+print (my_module.add(1,4))
+print(my_module.subtract(5,2))
---
+++ my_module.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def result(a,b):
+    return(my_module)
